00dbac593c31a81856ed17a23989dd4d8bdc26f6
Andy McCall <mailme@andymccall.co.uk>
2024-12-21 17:37:51 +0000

    chore(): further work

--- src/bitriotdev.asm
+++ src/bitriotdev.asm
@@ -1,8 +1,8 @@
 ;
 ; Title:		       Bitroit.dev
 ;
-; Description:         
-; Author:		       Andy McCall, mailme@andymccall.co.uk, others welcome!
+; Description:         Simple splash screen for Bitriot.dev
+; Author:		       Andy McCall, andy@bitriot.dev, others welcome!
 ;
 ; Created:		       2024-11-25 @ 17:28
 ; Last Updated:	       2024-11-25 @ 17:28
@@ -20,6 +20,7 @@
 
 ; API includes
     include "src/includes/system/mos_api.inc"
+    include "src/includes/api/macro_stack.inc"
     include "src/includes/api/vdu.inc"
     include "src/includes/api/vdu_screen.inc"
     include "src/includes/api/vdu_color.inc"
@@ -38,11 +39,7 @@
     include "src/includes/app/vdu_data.inc"
 
 start:
-    push af
-    push bc
-    push de
-    push ix
-    push iy
+    macro_stack_push_all
 
     call vdu_buffer_clear_all
 
@@ -57,6 +54,99 @@ start:
     call vdu_vblank
 
     call vdu_refresh
+
+    ;call play_tone
+
+    ld a, mos_getkbmap
+	rst.lil $08
+
+    ; Sending a VDU byte stream containing the logo
+    ld hl, vdu_logo_data
+    ld bc, vdu_logo_data_end - vdu_logo_data
+    rst.lil $18
+
+app_loop:
+
+    MOSCALL $1E                         ; load IX with keymap address
+
+    ; If the Escape key is pressed
+    ld a, (ix + $0E)    
+    bit 0, a
+    jp nz, quit
+
+    ; If the F key is pressed
+    ld a, (ix + $08)    
+    bit 3, a
+    call nz, show_full_screen
+
+    ; If the C key is pressed
+    ld a, (ix + $0A)
+    bit 1, a
+    call nz, show_small_screen
+
+    ; If the C key is pressed
+    ld a, (ix + $0A)
+    bit 2, a
+    call nz, clear_screen
+
+    call vdu_vblank
+
+    call vdu_refresh
+
+    jp app_loop
+
+show_full_screen:
+    call clear_screen
+    call full_screen
+    call play_tone
+    ret
+
+show_small_screen:
+    call clear_screen
+    call small_screen
+    ret
+
+quit:
+
+    ld hl, VDU_MODE_640x480x4_60HZ
+    call vdu_screen_set_mode
+
+    call vdu_cursor_flash
+
+    ld hl, quit_msg
+    call vdu_text_print
+
+    macro_stack_pop_all
+
+    ld hl,0
+
+    ret
+
+quit_msg:
+    .db "That was Bitriot.dev, impressive, huh??",13,10,0
+
+play_tone:
+    ld hl, tone
+    ld bc, end_tone - tone
+    rst.lil $18
+    ret 
+
+tone:  
+    .db 23,0,$85
+    .db 0
+    .db 4,3
+
+    .db 23,0,$85
+    .db 0
+    .db 0,63
+    .dw 1000
+    .dw 1500
+end_tone:
+
+
+full_screen:
+
+    push ix
 
 ; First column of long bars
     ld a, VDU_COL_WHITE
@@ -75,7 +165,7 @@ start:
     ld iy, 299
     call vdu_plot_filled_rect
 
-    ld a, VUD_COL_BRIGHT_CYAN
+    ld a, VDU_COL_BRIGHT_CYAN
     call vdu_plot_set_fill
     ld bc, 128
     ld de, 0
@@ -91,7 +181,7 @@ start:
     ld iy, 299
     call vdu_plot_filled_rect
 
-    ld a, 0x0D
+    ld a, VDU_COL_BRIGHT_MAGENTA
     call vdu_plot_set_fill
     ld bc, 256
     ld de, 0
@@ -106,7 +196,7 @@ start:
     ld ix, 383
     ld iy, 299
     call vdu_plot_filled_rect
-
+    
     ld a, VDU_COL_BRIGHT_BLUE
     call vdu_plot_set_fill
     ld bc, 384
@@ -148,7 +238,7 @@ start:
     ld iy, 320
     call vdu_plot_filled_rect
 
-    ld a, VUD_COL_BRIGHT_CYAN
+    ld a, VDU_COL_BRIGHT_CYAN
     call vdu_plot_set_fill
     ld bc, 287
     ld de, 300
@@ -231,66 +321,196 @@ start:
     ld iy, 120
     call vdu_plot_filled_rect
 
-; Sending a VDU byte stream containing the logo
-    ld hl, vdu_logo_data
-    ld bc, vdu_logo_data_end - vdu_logo_data
-    rst.lil $18
+    ld hl, BITRIOTDEV_FULL_LOGO_ID
+    call vdu_buffer_select
+    ld bc, 64
+    ld de, 90
+    call vdu_bitmap_plot
 
-    call play_tone
-
-
-app_loop:
-      
-    ld a, mos_getkbmap
-	rst.lil $08
-
-    ; If the Escape key is pressed
-    ld a, (ix + $0E)    
-    bit 0, a
-    jp nz, quit
-
-    call vdu_vblank
-
-    call vdu_refresh
-
-    jp app_loop
-
-quit:
-
-    ld hl, VDU_MODE_640x480x4_60HZ
-    call vdu_screen_set_mode
-
-    call vdu_cursor_flash
-
-    ld hl, quit_msg
-    call vdu_text_print
-
-    pop iy
     pop ix
-    pop de
-    pop bc
-    pop af
-    ld hl,0
 
     ret
 
-quit_msg:
-    .db "That was Bitriot.dev, impressive, huh??",13,10,0
+small_screen:
 
-play_tone:
-    ld hl, tone
-    ld bc, end_tone - tone
-    rst.lil $18
-    ret 
 
-tone:  
-    .db 23,0,$85
-    .db 0
-    .db 4,3
+; First column of long bars
+    ld a, VDU_COL_WHITE
+    call vdu_plot_set_fill
+    ld bc, 100
+    ld de, 100
+    ld ix, 139
+    ld iy, 199
+    call vdu_plot_filled_rect
 
-    .db 23,0,$85
-    .db 0
-    .db 0,63
-    .dw 1000
-    .dw 1500
-end_tone:
+    ld a, VDU_COL_BRIGHT_YELLOW
+    call vdu_plot_set_fill
+    ld bc, 140
+    ld de, 100
+    ld ix, 179
+    ld iy, 199
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_BRIGHT_CYAN
+    call vdu_plot_set_fill
+    ld bc, 180
+    ld de, 100
+    ld ix, 219
+    ld iy, 199
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_BRIGHT_GREEN
+    call vdu_plot_set_fill
+    ld bc, 220
+    ld de, 100
+    ld ix, 259
+    ld iy, 199
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_BRIGHT_MAGENTA
+    call vdu_plot_set_fill
+    ld bc, 260
+    ld de, 100
+    ld ix, 299
+    ld iy, 199
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_BRIGHT_RED
+    call vdu_plot_set_fill
+    ld bc, 300
+    ld de, 100
+    ld ix, 339
+    ld iy, 199
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_BRIGHT_BLUE
+    call vdu_plot_set_fill
+    ld bc, 340
+    ld de, 100
+    ld ix, 379
+    ld iy, 214
+    call vdu_plot_filled_rect
+
+; Second row of blocks of narrow bars
+    ld a, VDU_COL_BRIGHT_RED
+    call vdu_plot_set_fill
+    ld bc, 100
+    ld de, 200
+    ld ix, 159
+    ld iy, 214
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_WHITE
+    call vdu_plot_set_fill
+    ld bc, 160
+    ld de, 200
+    ld ix, 219
+    ld iy, 214
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_BRIGHT_YELLOW
+    call vdu_plot_set_fill
+    ld bc, 220
+    ld de, 200
+    ld ix, 279
+    ld iy, 214
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_BRIGHT_CYAN
+    call vdu_plot_set_fill
+    ld bc, 280
+    ld de, 200
+    ld ix, 339
+    ld iy, 214
+    call vdu_plot_filled_rect
+
+; Third row of squares
+    ld a, VDU_COL_DARK_BLUE
+    call vdu_plot_set_fill
+    ld bc, 100
+    ld de, 215
+    ld ix, 139
+    ld iy, 254
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_BLACK
+    call vdu_plot_set_fill
+    ld bc, 140
+    ld de, 215
+    ld ix, 179
+    ld iy, 254
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_DARK_MAGENTA
+    call vdu_plot_set_fill
+    ld bc, 180
+    ld de, 215
+    ld ix, 219
+    ld iy, 254
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_BLACK
+    call vdu_plot_set_fill
+    ld bc, 220
+    ld de, 215
+    ld ix, 259
+    ld iy, 254
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_DARK_GREY
+    call vdu_plot_set_fill
+    ld bc, 260
+    ld de, 215
+    ld ix, 299
+    ld iy, 254
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_WHITE
+    call vdu_plot_set_fill
+    ld bc, 300
+    ld de, 215
+    ld ix, 312
+    ld iy, 254
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_MID_GREY
+    call vdu_plot_set_fill
+    ld bc, 313
+    ld de, 215
+    ld ix, 326
+    ld iy, 254
+    call vdu_plot_filled_rect
+
+    ld a, VDU_COL_DARK_GREY
+    call vdu_plot_set_fill
+    ld bc, 327
+    ld de, 215
+    ld ix, 339
+    ld iy, 254
+    call vdu_plot_filled_rect
+
+; Black stripe
+    ld a, VDU_COL_BLACK
+    call vdu_plot_set_fill
+    ld bc, 100
+    ld de, 130
+    ld ix, 379
+    ld iy, 150
+    call vdu_plot_filled_rect
+
+   ld hl, BITRIOTDEV_SMALL_LOGO_ID
+    call vdu_buffer_select
+    ld bc, 140
+    ld de, 135
+    call vdu_bitmap_plot
+
+    pop ix
+
+    ret
+
+clear_screen:
+    ld a, quit_msg
+    call vdu_text_print
+    ld a, 16
+    rst.lil $10
+    ret
